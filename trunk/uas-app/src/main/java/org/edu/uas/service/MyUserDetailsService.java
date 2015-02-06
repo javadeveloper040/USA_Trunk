0182e15d10b42d3af336d492f4d0f3e1310e5c07
javadeveloper040 <javadeveloper040@b377e205-4f66-4d78-a9e5-4f7643ddd174>
2015-02-06 18:04:04 +0000

    Spring security configuration changes

---
+++ trunk/uas-app/src/main/java/org/edu/uas/service/MyUserDetailsService.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.edu.uas.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ *
+ * @author Pandu.Simbohtu
+ */
+public class MyUserDetailsService implements UserDetailsService{
+
+    public UserDetails loadUserByUsername(String string) throws UsernameNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
