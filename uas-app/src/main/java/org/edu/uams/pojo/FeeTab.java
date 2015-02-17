c50de8c40fc0a8de39e0979317f3ff09be04d9d2
javadeveloper040 <javadeveloper040@gmail.com>
2015-02-17 18:31:55 +0530

    [pandu] adding primary key

--- uas-app/src/main/java/org/edu/uams/pojo/FeeTab.java
+++ uas-app/src/main/java/org/edu/uams/pojo/FeeTab.java
@@ -12,6 +12,9 @@ import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
@@ -39,6 +42,18 @@ import javax.xml.bind.annotation.XmlRootElement;
     @NamedQuery(name = "FeeTab.findByOrigAmount", query = "SELECT f FROM FeeTab f WHERE f.origAmount = :origAmount")})
 public class FeeTab implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
     @EmbeddedId
     protected FeeTabPK feeTabPK;
     @Basic(optional = false)
